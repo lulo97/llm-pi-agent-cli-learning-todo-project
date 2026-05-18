890cce3eafa9909dd9611f8c55091f89ec7a8831
luongpysl2 <luongpysl2@gmail.com>
2026-05-18 22:28:49 +0700

    add llama server

---
+++ llama.cpp-server/run.bat
@@ -0,0 +1,10 @@
+llama-server.exe ^
+  -m Opus4.7-Distill-GODsGhost-Codex-4B-Q5_K_M.gguf ^
+  -c 20000 ^
+  -ngl all ^
+  -t 12 ^
+  -tb 12 ^
+  -fa auto ^
+  -np 1 ^
+  --host 127.0.0.1 ^
+  --port 8080
---
+++ project-1-simple-use/README.md
@@ -0,0 +1,3 @@
+Make a tasks folder and put multiple .md files
+Each md file is a small task that LLM can done under 1 minute
+After a task done then review and commit
